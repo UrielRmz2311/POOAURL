254465d39e9ea312f415257a72d8995da5b08221
UrielRmz2311 <125678140+UrielRmz2311@users.noreply.github.com>
2023-02-20 23:32:31 -0600

    Solución de errores en print (falto el simbolo +)

--- Personaje.py
+++ Personaje.py
@@ -1,21 +1,21 @@
 class Personaje:
     #atributos Personaje
     especie= "Humano"
-    nombre= "Mater chief"
+    nombre= "Master chief"
     altura= "2.70"
     
     #metodos Personaje
     def correr(self,status):
         if(status):
-            print("el personaje" +self.nombre " esta corriendo")
+            print("El personaje " + self.nombre + "esta corriendo")
         else:
-            print("El personaje"+ self.nombre " se detuvo")
+            print("El personaje "+ self.nombre +" se detuvo")
                          
     def lanzarGranadas(self):
-         print("El personaje"+ self.nombre " lanzo una granada")
+         print("El personaje "+ self.nombre +" lanzo una granada")
            
-    def recargarArma(self,municiones):
+    def recargarArma(self, municiones):
         cargador= 10
         cargador= cargador + municiones
-        print("El arma tiene"+ cargador "balas")
+        print("El arma tiene "+ str(cargador) +" balas")
 
