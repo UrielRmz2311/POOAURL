580f7c24b96ca117f7d0df75c02b21b9b520518f
UrielRmz2311 <125678140+UrielRmz2311@users.noreply.github.com>
2023-03-01 16:21:37 -0600

    Funciones con messagebox y agregar botones

--- tkinther/practica11.py
+++ tkinther/practica11.py
@@ -1,11 +1,18 @@
 from tkinter import Tk, Button ,Frame, messagebox
 
+
+#5.Creamos la funcion crear mensajes
 def mostrarMensajes():
     messagebox.showinfo("showinfo", "Presionaste el Botón Azul")
+    messagebox.showerror("Error","Todo fallo con exito")
+    print(messagebox.askyesnocancel("Pregunta", "¿Ella jugó con tu corazón?"))
 
-
-
-
+#6. Función para agregar botones
+def agregarBoton():
+    botonVerde.config(text="+", bg="green", fg="white")
+    botonNuevo=Button(seccion3,text="Nuevo")
+    botonNuevo.pack()
+ 
 #1. Instanciamos el objeto ventana
 
 ventana= Tk()
@@ -33,7 +40,7 @@ botonAmarillo.grid(row= 0, column=0)
 botonNegro=Button(seccion2,text="Botón Negro",bg="black",fg="white")
 botonNegro.grid(row = 1, column=1)
 
-botonVerde=Button(seccion3,text="Botón Verde",bg="green", fg="white")
+botonVerde=Button(seccion3,text="Botón Verde",bg="green", fg="white",command=agregarBoton)
 botonVerde.configure(height=2 , width=10)
 botonVerde.pack()
 
