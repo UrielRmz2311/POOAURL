7861c0179f85f35f0ab8ca4ea5333b22e40efa8b
UrielRmz2311 <125678140+UrielRmz2311@users.noreply.github.com>
2023-03-15 16:32:58 -0600

    Se genero la interfaz y unos ejemplos de practicas pasadas

---
+++ Examen2doP/Matricula.py
@@ -0,0 +1,22 @@
+from tkinter import *
+Interfaz=Tk()
+Interfaz.title("Generador de Matricula")
+Interfaz.geometry("400x400")
+
+
+
+seccion = Frame(Interfaz,width=400,height=300)
+seccion.pack()
+seccion.config(bg ="lightblue")
+seccion.config(relief="sunken")
+seccion.config(bd=25)
+
+seccion2= Frame(Interfaz,bg="lightblue") #bg es opcional
+seccion2.pack(expand=True, fill='both')
+
+label = Label(Interfaz, text="Genere su Matricula")
+label.pack(anchor=CENTER)
+label.config(fg="blue",    
+            bg="lightblue",  
+            font=("Verdana",24)) 
+Interfaz.mainloop()
---
+++ solicitud.py
@@ -0,0 +1,8 @@
+
+
+Nombre=str(input("Escriba su nombre: "))
+ApellidoP=str(input("Escriba su apellido paterno: "))
+ApellidoM=str(input("Escriba su apellido materno: "))
+AñoNac=int(input("Escriba el año de nacimiento: "))
+Carrera=str(input("Escriba las siglas de su carreara: "))
+
