edb78a58d25e9e351f49d6bba92b8e860523603c
UrielRmz2311 <125678140+UrielRmz2311@users.noreply.github.com>
2023-03-15 17:39:20 -0600

    archivo generador de carpeta Examen2doPA crea lo que se pidio

--- Examen2doPA/generador.py
+++ Examen2doPA/generador.py
@@ -1,3 +1,4 @@
+import random
 Nombre=str(input("Escriba su nombre: "))
 ApellidoP=str(input("Escriba su apellido paterno: "))
 ApellidoM=str(input("Escriba su apellido materno: "))
@@ -11,7 +12,8 @@ am=ApellidoM[0:3]
 an=AñoNac[2:4]
 anc=Añocur[2:4]
 ca=Carrera[0:3]
+Nume=str(random.randint(100,999))
 
-cantidad=ca+anc+an+nom+ap+am
-print(anc)
-print(cantidad)
+cantidad=ca+anc+an+nom+ap+am+Nume
+canti=cantidad.upper()
+print(canti)
